5088e2fef8c79af7c929146f0aef2de4fd0ebc8c
Mats Fockaert <38748520+dutie@users.noreply.github.com>
2020-11-15 16:17:15 +0100

    Small edit to portals encapsulation

--- src/portals/ThreeQuartersTransformation.java
+++ src/portals/ThreeQuartersTransformation.java
@@ -7,19 +7,24 @@ public class ThreeQuartersTransformation implements Transformation{
 	 * @invar | fraction <= 2.0
 	 * @immutable
 	 */
-	public final double fraction = (3.0/4.0);
+	private final double fraction = (3.0/4.0);
 	
 	/**
 	 * Returns a size of an object scaled by the portal's transformation
 	 * 
 	 * @pre  | 0 < objSize
 	 * 
-	 * @post | result == Math.round(objSize * fraction)
+	 * @post | result == Math.round(objSize * getFraction())
 	 */
 	@Override
 	public int apply(int objSize) {
 		return (int) Math.round(objSize * fraction);
 	}
+	
+	public double getFraction() {
+		return fraction;
+	}
+	
 
 
 	
--- src/portals/TwoThirdsTransformation.java
+++ src/portals/TwoThirdsTransformation.java
@@ -6,18 +6,22 @@ public class TwoThirdsTransformation implements Transformation{
 	 * @invar | fraction <= 2.0
 	 * @immutable
 	 */
-	public final double fraction = (2.0/3.0);
+	private final double fraction = (2.0/3.0);
 	
 	/**
 	 * @throws IllegalArgumentException if objSize is negative or zero
 	 *     | 0 >= objSize
-	 * @post | result == Math.round(objSize * fraction)
+	 * @post | result == Math.round(objSize * getFraction())
 	 */
 	@Override
 	public int apply(int objSize) {
 		if(0>objSize)
 			throw new IllegalArgumentException("Size of object must be larger than 0");
 		return (int) Math.round(objSize * fraction);
+	}
+	
+	public double getFraction() {
+		return fraction;
 	}
 	
 }
